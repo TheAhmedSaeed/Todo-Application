35bb0dda26549d101c17f0ee628d99156003bc83
ahmmedms <mrmoood@hotmail.com>
2021-09-23 21:20:22 +0300

    used tailwind for css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,33 @@
 const { guessProductionMode } = require("@ngneat/tailwind");
+const colors = require("tailwindcss/colors");
 
-process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
+process.env.TAILWIND_MODE = guessProductionMode() ? "build" : "watch";
 
 module.exports = {
-    prefix: '',
-    mode: 'jit',
-    purge: {
-      content: [
-        './src/**/*.{html,ts,css,scss,sass,less,styl}',
-      ]
+  prefix: "",
+  mode: "jit",
+  purge: {
+    content: ["./src/**/*.{html,ts,css,scss,sass,less,styl}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
     },
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/typography')],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
 };
